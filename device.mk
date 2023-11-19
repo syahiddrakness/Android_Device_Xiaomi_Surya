79ded5ad332da804fa78c73a142d76da3c46380a
rdxzv <rdxzv.dev@gmail.com>
2023-11-19 12:08:36 +0000

    surya: import XiaomiParts
    HEAD: https://github.com/rd-stuffs/android_device_xiaomi_surya/commit/9d00f7bd6fd1eb7628c6e1ee32a0b7321140f7a0
    
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# XiaomiParts
+$(call inherit-product, device/xiaomi/surya/parts/parts.mk)
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
