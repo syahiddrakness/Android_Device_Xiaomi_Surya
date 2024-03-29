080c4a8aca7f130b4b882c92e58ee792ff2e45ae
syahiddraakness <syahidselaluspd708@gmail.com>
2024-03-30 05:03:52 +0700

    surya: BringUp Evolution X

--- evolution_surya.mk
+++ evolution_surya.mk
@@ -15,11 +15,18 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 $(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 EVO_BUILD_TYPE := Community
 
-# Device identifier.
+# Spec Device identifier.
 PRODUCT_NAME := evolution_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Target Features Device
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_PICO_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDES_MIUI_CAMERA := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
